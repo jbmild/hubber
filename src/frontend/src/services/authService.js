06a34e737525018adf381004df7c8aa392d218fd
muratoredante <muratoredante@yahoo.com>
2024-08-01 21:41:15 -0300

    Se agrega Google OAuth. Pagina perfil como ejemplo de que toma los datos

--- src/frontend/src/services/authService.js
+++ src/frontend/src/services/authService.js
@@ -33,14 +33,14 @@ export function authStandard (data, callback) {
 }
 
 export function authOauth (provider) {
-    const clientId = provider === 'google' ? 'YOUR_GOOGLE_CLIENT_ID' : 'YOUR_IOS_CLIENT_ID'; // estos van en archivo de cfg
-    const redirectUri = `http://localhost:5000/auth/callback`; // redireccionar al back para que verifique con oauth y setee la cookie del token/codigo
+    const clientId = provider === 'google' ? '324276260857-smhaobomhaahqqaa1j20t88t8haqnnmk.apps.googleusercontent.com' : 'YOUR_IOS_CLIENT_ID'; // estos van en archivo de cfg
+    const redirectUri = `http://localhost:3000/auth/callback`; // redireccionar al back para que verifique con oauth y setee la cookie del token/codigo
     const responseType = 'code';
     const scope = 'profile email';
 
-    const authUrl = `https://accounts.${provider}.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
+    //const authUrl = `https://accounts.${provider}.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
 
-    window.location.href = authUrl;
+    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/auth/google`;
 }
 
 export async function createUser (data) {

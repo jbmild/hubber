c62d84335e8dc08fce1ea25cabea0887588205ef
smchejolan <smchejolan@gmail.com>
2024-08-04 23:48:55 -0300

    Agregadas opciones de prueba al chat

--- src/frontend/src/components/chatbot/ActionProvider.jsx
+++ src/frontend/src/components/chatbot/ActionProvider.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  
+  const addMessageToState = (message) => {
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+    }));
+  };
+
+  
+  const handlePais = () => {
+    const message = createChatBotMessage('Seleccione un pais',
+      {        
+        withAvatar: true,
+      }
+    );
+
+    addMessageToState(message)
+  }
+
+  const handleNormativasBasicas = () => {
+    const message = createChatBotMessage('Se le guiara por normaticas basicas',
+      {        
+        withAvatar: true,
+      }
+    );
+
+    addMessageToState(message)
+  }
+  
+
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: {
+            handlePais,
+            handleNormativasBasicas
+          },
         });
       })}
     </div>
--- src/frontend/src/components/chatbot/config.js
+++ src/frontend/src/components/chatbot/config.js
@@ -1,11 +1,30 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
+import GeneralOptions from './OptionSelector/GeneralOptions';
 
 const botName = 'BOTSITO';
 
 const config = {
-  initialMessages: [createChatBotMessage(`Hola soy ${botName}, ¿cómo puedo ayudarte hoy?`)],
+  initialMessages: [
+    createChatBotMessage(`Hola soy ${botName}, ¿cómo puedo ayudarte hoy?`),
+    createChatBotMessage('Acá hay posibles opciones de conversación.',
+      {
+        widget: "options",
+        delay: 500,
+        withAvatar: true,
+      }
+    )
+  ],
   botName: botName,
-  
+  lang: 'es',
+  state: {
+  },
+  widgets: [
+    {
+      widgetName: "options",
+      widgetFunc: (props) => <GeneralOptions {...props} />,     
+      mapStateToProps: ["messages"],
+    },
+  ]
 };
 
 export default config;

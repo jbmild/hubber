cfcaa24192aa52a7517b78104119a048f020a7db
Dzukito <30675879+Dzukito@users.noreply.github.com>
2024-11-12 20:41:55 -0300

    logochat

--- src/frontend/src/components/chatbot/Components/BotIcon/index.js
+++ src/frontend/src/components/chatbot/Components/BotIcon/index.js
@@ -2,5 +2,5 @@ import { useEffect, useState } from 'react';
 import { Avatar, Badge } from '@mui/material';
 
 export default function IconoBot() {
-    return (<Avatar src='/images/logo.png' alt='H'/>)
+    return (<Avatar src='/images/logochat.png' alt='H'/>)
 }
